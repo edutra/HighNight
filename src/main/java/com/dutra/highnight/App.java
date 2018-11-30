74a394077c8fcade5aa9a620cf22cbb10d04e148
Guaele <ebodutra@gmail.com>
2018-11-29 23:45:41 -0200

    first commit

---
+++ src/main/java/com/dutra/highnight/App.java
@@ -0,0 +1,13 @@
+package com.dutra.highnight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
